1c0633ca9b0ab37427fe2a81eeda7cfb87485d36
ImThePeak <99116977+venom-mancer@users.noreply.github.com>
2022-11-19 17:34:53 +0330

    Update csv_TO_1.py

--- csv_TO_1.py
+++ csv_TO_1.py
@@ -1,15 +1,16 @@
+#needed librarys 
 import pandas as pd
 import csv
 
 
-df = pd.read_csv(r'C:\Users\APA\Desktop\Email_1401.csv')
-df1 = df[['description', 'name','family','password','email id']]
+df = pd.read_csv(r'Path to csv') #CSV File location
+df1 = df[['description', 'name','family','password','email id']] #change this based on your csv header or csv column title
 
-header = ['data']
-with open(r'C:\Users\APA\Desktop\export_csv.csv', 'w' , encoding='UTF8') as f:
+header = ['data'] #give the title you want to import all columns to it
+with open(r'Path to csv', 'w' , encoding='UTF8') as f:
 
     writer = csv.writer(f)
-
+    # CREATING HEADER
     writer.writerow(header)
-    
+    # WRITING DATA TO 1 ROW
     writer.writerow(df1.values)
